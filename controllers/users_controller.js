72d246d926cd06705442cba1c29c493a56cc2e01
Kesava Etcherla <kesava1020@gmail.com>
2022-07-21 01:51:27 +0530

    setup statis files access for layouts and pages

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -2,7 +2,7 @@ module.exports.profile=function(request,response){
     
     // return response.end('<h1>User profile!</h1>');
 
-    return response.render('users',{
+    return response.render('users_profile',{
         title: "profile"
     });
 };
--- index.js
+++ index.js
@@ -2,7 +2,15 @@
 const express= require('express');
 const app=express();
 const port =8000;
+//add express ejs library for layouts
+const expressLayouts= require('express-ejs-layouts');
 
+app.use(express.static('./assets'));
+
+app.use(expressLayouts);
+// extracts style and scripts from sub pages into the layout
+app.set('layout extactStyles', true);
+app.set('layout extactScripts', true);
 
 //use express router
 app.use('/', require('./routes'));
