49e2d013def8eb2c5ac8b40cad67ae0be615baf0
Kesava Etcherla <kesava1020@gmail.com>
2022-07-20 20:48:33 +0530

    added a controller and a router for home

---
+++ controllers/home_controller.js
@@ -0,0 +1,5 @@
+module.exports.home=function(request,response){
+    return response.end('<h1>Express is up for codeial!</h1>');
+};
+
+//modules.exports.actionname= function(request,response){...};
