de6cc57afa51afbe6b1108792e4023a088ad04e6
Kesava Etcherla <kesava1020@gmail.com>
2022-08-04 23:53:07 +0530

    added node-sass-middleware

--- index.js
+++ index.js
@@ -13,6 +13,15 @@ const passport=require('passport');
 const passportLocal =require('./config/passport-local-strategy');
 //used tto store the session cookie
 const MongoStore = require('connect-mongo')(session);
+const sassMiddleware= require('node-sass-middleware');
+
+app.use(sassMiddleware({
+    src: '/assets/scss',
+    dest: '/assets/css',
+    debug: true,
+    outputStyle: 'extended',
+    prefix: '/css'
+}));
 
 // app.use(express.urlencoded()); depriciated
 const bodyParser= require('body-parser');
